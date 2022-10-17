6b68d10c335bdfa8bb2df63169efb5807f976dfa
Isaac Berrio <berriisaax18@hotmail.com>
2022-10-17 10:30:05 -0500

    commit #2

--- scipt.js
+++ scipt.js
@@ -1,15 +1,18 @@
 alert("Sistema de calificacion para alumnos")
 
 let nombreIngresado = prompt("Cual es tu nombre??")
-let nota1 = Number(prompt("Ingrese la nota numero 1"))
-let nota2 = Number(prompt("Ingrese la nota numero 2"))
-let nota3 = Number(prompt("Ingrese la nota numero 3"))
-let nota4 = Number(prompt("Ingrese la nota numero 4"))
-let nota5 = Number(prompt("Ingrese la nota numero 5"))
+pedirNumeros()
 
 
 let resultadoNota = ((nota1 + nota2 + nota3 + nota4 + nota5) / 5)
 
+function pedirNumeros() {
+    nota1 = Number(prompt("Ingrese la nota numero 1"))
+    nota2 = Number(prompt("Ingrese la nota numero 2"))
+    nota3 = Number(prompt("Ingrese la nota numero 3"))
+    nota4 = Number(prompt("Ingrese la nota numero 4"))
+    nota5 = Number(prompt("Ingrese la nota numero 5"))
+}
 function notaFinal(nombre,nota) {
     
     if(nota >= 3) {
